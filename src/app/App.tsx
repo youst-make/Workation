9235f8a024a0e12c834ebc9cccbca090e1befbeb
Youstyna <justynaumwawa@gmail.com>
2026-05-06 15:57:41 +0200

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -123,17 +123,17 @@ function StartSection() {
       <h1>Workation 🌿</h1><br>
       <h2>Podstawowe informacje</h2>
       <p>Wyjazd w piątek rano, wracamy wg preferencji.
-         Jedziemy na jeden dzień poza biuro — do miejsca, gdzie pracuje się lepiej, oddycha głębiej i myśli jaśniej :D
-         Wyruszamy na jednodniowe workation do Stodoły Artystów — przestrzeni stworzonej do kreatywnej pracy, rozmów i złapania nowej perspektywy.
+         Jedziemy na jeden dzień poza biuro — do miejsca, gdzie pracuje się lepiej, oddycha głębiej i myśli jaśniej :D<br>
+         Wyruszamy na jednodniowe workation do Stodoły Artystów — przestrzeni stworzonej do kreatywnej pracy, rozmów i złapania nowej perspektywy.<br>
          To nie tylko wyjazd integracyjny. To dzień, w którym łączymy pracę, współpracę i reset w jednym miejscu.</p><br>
 
-      <h3>Co nas czeka?</h3>
-      <p><ul>Co nas czeka?
-        <li>wspólna podróż i luźny start dnia</li>
-        <li>czas na pracę w spokojnym, inspirującym otoczeniu</li>
-        <li>przerwy na kawę, jedzenie i rozmowy bez Slacka</li>
-        <li>przestrzeń na pomysły, które nie mieszczą się w biurze</li>
-        <li>trochę chaosu, trochę śmiechu i dużo świeżego powietrza</li></ul></p>
+      <h2>Co nas czeka?</h2>
+      <p>
+        <li>● wspólna podróż i luźny start dnia</li>
+        <li>● czas na pracę w spokojnym, inspirującym otoczeniu</li>
+        <li>● przerwy na kawę, jedzenie i rozmowy bez Slacka</li>
+        <li>● przestrzeń na pomysły, które nie mieszczą się w biurze</li>
+        <li>● trochę chaosu, trochę śmiechu i dużo świeżego powietrza</li></ul></p>
     `);
   }, []);
 
